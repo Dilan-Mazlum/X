407d485e1181c806fd297b8c8c106b2b0c121d3e
Dilan Mazlum <dilanmazlum513@gmail.com>
2024-06-07 16:16:40 +0300

    First commit

--- Js/x.js
+++ Js/x.js
@@ -1,2 +1,61 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var buttons = [
+        {buttonId: 'chatButton', countId: 'chatCount'},
+        {buttonId: 'repeatButton', countId: 'repeatCount'},
+        {buttonId: 'favoriteButton', countId: 'favoriteCount'},
+        {buttonId: 'chartButton', countId: 'chartCount'},
+        {buttonId:'chatButton1', countId:'chatCount1'},
+        {buttonId: 'repeatButton1', countId: 'repeatCount1'},
+        {buttonId: 'favoriteButton1', countId: 'favoriteCount1'},
+        {buttonId: 'chartButton1', countId: 'chartCount1'},
+        {buttonId:'chatButton2', countId:'chatCount2'},
+        {buttonId: 'repeatButton2', countId: 'repeatCount2'},
+        {buttonId: 'favoriteButton2', countId: 'favoriteCount2'},
+        {buttonId: 'chartButton2', countId: 'chartCount2'},       
+    ];
+  
+
+    buttons.forEach(function(item) {
+        var button = document.getElementById(item.buttonId);
+        if (button) {
+            button.addEventListener('click', function() {
+                increaseCount(item.countId, this);
+            });
+        } else {
+            console.error(`Element with ID ${item.buttonId} not found`);
+        }
+    });
+
+    function increaseCount(countId, element) {
+        var countElement = document.getElementById(countId);
+        if (countElement) {
+            var count = parseInt(countElement.textContent);
+            count++;
+            countElement.textContent = count;
+
+        } else {
+            console.error(`Element with ID ${countId} not found`);
+        }
+    }
+});
+
+
+const happend=document.querySelector('#wht');
+happend.addEventListener("focus",whtHappend);
+
+function whtHappend(){
+    console.log('odaklandı');
+
+};
+
+const sendBtn = document.querySelector('.btn-what-send');
+const happen=document.querySelector('#wht');
+
+happen.addEventListener('click',function(){
+   sendBtn.style.backgroundColor='rgb(29, 155, 240)';
+   sendBtn.style.cursor='pointer';
+   console.log('Submit');
+});
+
 
 
